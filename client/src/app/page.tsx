848cf761d3e858954459e27683b3fd2147aed3db
tolgabp <tolgabarisp@gmail.com>
2025-08-03 16:17:40 +0100

    setup finished

---
+++ client/src/app/page.tsx
@@ -0,0 +1,17 @@
+import SignupPage from "./signup/page";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          <header>dev-tasks</header>
+        </div>
+        <footer>
+          Developed by TBP
+        </footer>
+      </main>
+    </div>
+  );
+}
+
